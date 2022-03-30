008d65ca0beaa1dbca70fef9579b48dd483734cf
michael-nevius <michael.a.nevius@gmail.com>
2022-03-30 10:58:52 -0400

    Finished the second problem for the peanut butter jelly time

--- problem_solving.py
+++ problem_solving.py
@@ -1,6 +1,9 @@
-best_football_team = 'packers'
-for character in best_football_team:
-    print(character)
+# reversed_word = input('Please select a word: ')
+# for index in range(len('') -1, -1 ,1):
+#     reversed_word += ''[0, -1]
+    
+# print(reversed_word)
+
 
 def peanutbutterjellytime():
      user_input = 'n'
@@ -12,18 +15,20 @@ def peanutbutterjellytime():
             else:
                 print(f'Congrats you now get to see what I wanted to show you')
 
-     return user_input
-def secret_sauce():     
-    for number in range(0,101):
-        print(number)
-    if (number%15 == 0):
-        print('peanut butter jelly')
-    if (number%5 == 0):
-        print('jelly')
-    if (number%3 == 0):
-        print('peanut')
+user_input = peanutbutterjellytime()     
 
-user_input = peanutbutterjellytime()
-print(f'{secret_sauce}')
+
+print     
+for number in range(0,101):
+    print(number)
+if (number%15 == 0):
+    print('peanut butter jelly')
+if (number%5 == 0):
+    print('jelly')
+if (number%3 == 0):
+    print('peanut')
+
+print('Well was it worth it did you enjoy it?')
+
 
     
