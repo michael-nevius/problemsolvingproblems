376a65400a1954cb2a644b0e1493b667c4a8bdca
michael-nevius <michael.a.nevius@gmail.com>
2022-03-31 12:19:22 -0400

    Finished problem 6 on the first worksheet now i'm going to move onto the second workshee

--- problem_solving.py
+++ problem_solving.py
@@ -1,63 +1,63 @@
-def reversed_word_tool():
-    word = input('Please type a word that you would like to see reversed: ')
-    for index in range(len(word) -1, -1 ,1):
-            print(word[index])
+# def reversed_word_tool():
+#     word = input('Please type a word that you would like to see reversed: ')
+#     for index in range(len(word) -1, -1 ,1):
+#             print(word[index])
 
-    reversed_word = ''    
-    for index in range (len(word) -1, -1, -1):
-        reversed_word += word[index]
+#     reversed_word = ''    
+#     for index in range (len(word) -1, -1, -1):
+#         reversed_word += word[index]
 
-    print(reversed_word)
+#     print(reversed_word)
 
-user_input = reversed_word_tool()
+# user_input = reversed_word_tool()
 
 
-def peanutbutterjellytime():
-     user_input = 'n'
-     while user_input.lower() == 'n':
-            user_input = input(f'Would you like to see something really cool? y/n: ')
-            if user_input.lower() == 'n':
-                print('Well I didnt want to show you anyhow')
+# def peanutbutterjellytime():
+#      user_input = 'n'
+#      while user_input.lower() == 'n':
+#             user_input = input(f'Would you like to see something really cool? y/n: ')
+#             if user_input.lower() == 'n':
+#                 print('Well I didnt want to show you anyhow')
                
-            else:
-                print(f'Congrats you now get to see what I wanted to show you')
+#             else:
+#                 print(f'Congrats you now get to see what I wanted to show you')
 
-user_input = peanutbutterjellytime()     
+# user_input = peanutbutterjellytime()     
 
-print    
-for number in range(0,101):
-    print(number)
-    if (number%15 == 0):
-        print('peanut butter jelly')
-    if (number%5 == 0):
-        print('jelly')
-    if (number%3 == 0):
-        print('peanut')
+# print    
+# for number in range(0,101):
+#     print(number)
+#     if (number%15 == 0):
+#         print('peanut butter jelly')
+#     if (number%5 == 0):
+#         print('jelly')
+#     if (number%3 == 0):
+#         print('peanut')
 
-print('Well was it worth it did you enjoy it?')
+# print('Well was it worth it did you enjoy it?')
 
-word = input("Please input a word you would like to have indexed.: ")
+# word = input("Please input a word you would like to have indexed.: ")
 
-def final_word(word):
-    for number in range(len(word)):
-        print(number)
-        print(word[number])
+# def final_word(word):
+#     for number in range(len(word)):
+#         print(number)
+#         print(word[number])
 
-user_input = final_word(word)
-ingredients = ['pepperoni', 'anchovies', 'mushrooms', 'pineapple', 'green peppers', 'sausage']
-def search():
-    user_input = input(f'What ingredient would you like to add to your pizza?: ')
-    ingredient = ''
-    while user_input != ingredient:
-        for ingredient in ingredients:
+# user_input = final_word(word)
+# ingredients = ['pepperoni', 'anchovies', 'mushrooms', 'pineapple', 'green peppers', 'sausage']
+# def search():
+#     user_input = input(f'What ingredient would you like to add to your pizza?: ')
+#     ingredient = ''
+#     while user_input != ingredient:
+#         for ingredient in ingredients:
 
-            if user_input == ingredient:
-                print('That sounds like a good pizza!')
+#             if user_input == ingredient:
+#                 print('That sounds like a good pizza!')
                 
-            else:
-                input(f'Umm how about no try again: ')    
+#             else:
+#                 input(f'Umm how about no try again: ')    
                  
-pizza_time = search()
+# pizza_time = search()
 
 def Reverse(list):
     new_lst = list[::-1]
@@ -68,3 +68,9 @@ list = ['toyota', 'dodge', 'fiat', 'chevy', 'ford', 'porche', 'bicycle']
 print(Reverse(list))
 
 
+def nametool():
+    names = ['Rebecca', 'Sam', 'Bob', 'Dante', 'Monica', 'Brad']
+    for i in names:
+        if(len(i)) > 4:
+            print(i)
+nametool()
